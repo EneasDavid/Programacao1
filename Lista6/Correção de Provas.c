c675a606e9a793402cbd5f91ebe94c880f7ff1e7
Enéas <david00eneas@gmail.com>
2023-09-14 18:07:53 -0300

    adicao periodica, questão da correcao de prova

--- Lista6/Correção de Provas.c
+++ Lista6/Correção de Provas.c
@@ -2,74 +2,74 @@
 #define MAX 10
 #define MAXALUNO 100
 
-// Função para calcular a pontuação de um aluno
+//calcular a pontua??o de um aluno
 int correcao(char gabarito[], char resposta[], int contador) {
-    if (contador<MAX) {
-        if (gabarito[contador]==resposta[contador]) {
-            return 1+correcao(gabarito, resposta, contador+1);
+    if (contador < MAX) {
+        if (gabarito[contador] == resposta[contador]) {
+            return 1 + correcao(gabarito, resposta, contador + 1);
         }
-        return correcao(gabarito, resposta, contador+1);
+        return correcao(gabarito, resposta, contador + 1);
     }
     return 0;
 }
 
-// Função para preencher um array de caracteres
+//preencher um array de caracteres
 void preencher(char myArray[], int posicao) {
-    if (posicao<MAX) {
-        scanf(" %c", &myArray[posicao]); // Adicionei um espaço antes do %c para ignorar espaços em branco
-        preencher(myArray, posicao+1);
+    if (posicao < MAX) {
+        scanf(" %c", &myArray[posicao]); //espa?o antes do %c para ignorar espa?os em branco
+        preencher(myArray, posicao + 1);
     }
     return;
 }
 
-// Função principal para ler os dados dos alunos
-int main(){
-    char gabarito[MAX], provaSala[MAXALUNO][MAX];
-    int notasAlunos[MAXALUNO][2], acertos[MAXALUNO];
-
-    preencher(gabarito, 0);
-
+void myWhile(int notasAlunos[][2], int acertos[], char gabarito[], int frequenciaNotas[], int *contador){
     int entrada;
     scanf("%d", &entrada);
-
-    int contador = 0; // Adicionei um contador para controlar o número de alunos
-    int maiorNota = -1; // Adicionei uma variável para rastrear a maior nota
-    int frequenciaNotas[11] = {0}; // Adicionei um array para rastrear a frequência das notas
-
-    // Enquanto o número do aluno for diferente de 9999
-    while (entrada!=9999){
+    if(entrada!=9999){
         char resposta[MAX];
         preencher(resposta, 0);
-
         int pontuacao=correcao(gabarito, resposta, 0);
-        notasAlunos[contador][0]=entrada;
-        notasAlunos[contador][1]=pontuacao;
-        acertos[contador]=pontuacao;
-
-        // Verifica se a pontuação é maior que a maior pontuação registrada até agora
-        if (pontuacao>maiorNota) {
-            maiorNota=pontuacao;
-        }
-
-        // Registra a frequência da pontuação
+        notasAlunos[*contador][0]=entrada;
+        notasAlunos[*contador][1]=pontuacao;
+        acertos[*contador]=pontuacao;
+        //registra a frequ?ncia da pontua??o
         frequenciaNotas[pontuacao]++;
+        printf("%d %.1lf\n", notasAlunos[*contador][0], (double)notasAlunos[*contador][1]);
+        (*contador)++;
+        myWhile(notasAlunos, acertos, gabarito, frequenciaNotas, contador);
+    }    
+    return ;
+}
 
-        printf("%d %.1lf\n", notasAlunos[contador][0], (double)notasAlunos[contador][1]);
-
-        contador++;
-
-        scanf("%d", &entrada);
+int myFor(int notaBase, int repeticaoNota[]){
+    if(notaBase<11){
+        return repeticaoNota[notaBase]+myFor(notaBase+1, repeticaoNota);
     }
-
-    // Calcula a porcentagem de aprovação
-    int aprovados=0;
-    for (int i=6; i<=10; i++) {
-        aprovados+=frequenciaNotas[i];
+    return 0;
+}
+void percorrerArray(int myArray[], int loop, int *mais, int qtn){
+    if(loop==11){
+        return;
     }
-    double percentualAprovacao=(double)aprovados/contador*100.0;
+    if(myArray[loop]>=qtn){
+        *mais=loop;
+        qtn=myArray[loop];
+    }
+    percorrerArray(myArray,loop+1,mais,qtn);
+}
+
+int main() {
+    char gabarito[MAX], provaSala[MAXALUNO][MAX];
+    int notasAlunos[MAXALUNO][2], acertos[MAXALUNO], contador=0, aprovados=0, maisAparece=0,frequenciaNotas[11]={0};
+    preencher(gabarito, 0);
+    //fun??o pra ler as respostas dos cornos
+    myWhile(notasAlunos, acertos, gabarito, frequenciaNotas, &contador);
+    //pra calcular a porcentagem de aprova??o
+    aprovados=myFor(6,frequenciaNotas);
+    double percentualAprovacao = (double)aprovados/contador*100;
+    percorrerArray(frequenciaNotas, 0, &maisAparece, 0);
 
     printf("%.1lf%%\n", percentualAprovacao);
-    printf("%d.0\n", maiorNota);
-
+    printf("%d.0\n", maisAparece);
     return 0;
 }
