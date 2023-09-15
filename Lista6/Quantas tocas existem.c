4468da807afe1ebf7b2409d39d9f5838b59cb17a
Enéas <david00eneas@gmail.com>
2023-09-15 06:29:05 -0300

    adicao periodica, questao da toca

--- Lista6/Quantas tocas existem.c
+++ Lista6/Quantas tocas existem.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
-int encontreTocas(int arr[], int ind, int tamanho) {
-    if (ind==tamanho) {
-        if (arr[arr[tamanho-1]]==tamanho-1) {
-            return 1;
-        } else {
-            return 0;
-        }
+int osvaldo(int x, int tamanho, int v[]) {
+    if (x == tamanho) {
+        return 0;
     }
-    int contador = (arr[ind]==arr[tamanho-1])?1:0;
-    return contador + encontreTocas(arr, ind + 1, tamanho);
+    
+    int contagem = (v[x] != x) ? 1 : 0;
+
+    return contagem + osvaldo(x + 1, tamanho, v);
 }
 
-void myFor(int arr[], int ind, int tamanho) {
-    if (ind==tamanho) {
+void looping(int v[], int ind, int tamanho) {
+    if (ind == tamanho) {
         return;
     }
-    scanf("%d", &arr[ind]);
-    myFor(arr, ind+1, tamanho);
+    scanf("%d", &v[ind]);
+    looping(v, ind + 1, tamanho);
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    int connections[n];
-    myFor(connections, 0, n);
-    int tocas = encontreTocas(connections, 0, n);
-    printf("%d\n", tocas);
+    int v[n];
+    looping(v, 0, n);
+    printf("%d", osvaldo(0, n, v));
     return 0;
 }
--- Lista6/tempCodeRunnerFile.c
+++ Lista6/tempCodeRunnerFile.c
@@ -1,58 +1,30 @@
 #include <stdio.h>
 
-void calculandoFigurinhas(int myArray[], int tamanho, int *pontosMaria, int *pontosJoao, int i){
-    if(i<tamanho){
-        if(myArray[i]!=0){
-            if(myArray[i]%2==0){
-                *pontosJoao+=myArray[i];
-            }else{
-                *pontosMaria+=myArray[i];
-            }
-        }
-        calculandoFigurinhas(myArray, tamanho, pontosMaria, pontosJoao, i+1);
-    }
-}
-
-void repetida(int myArray[], int tamanho, int i, int j){
-    if(i<tamanho){
-        if(j<tamanho){
-            if(i!=j){
-                if(myArray[i]==myArray[j]){
-                    myArray[j]=0;
-                }
-            }
-            repetida(myArray, tamanho, i, j+1);
-        }
-        repetida(myArray, tamanho, i+1, 0);
-    }
-}
-
-void myFor(int myArray[], int tamanho, int *tamanhoJoao, int *tamanhoMaria, int loop){
-    if(loop<tamanho){
-        int figuinha;
-        scanf("%d", &figuinha);
-        myArray[loop] = figuinha; // Armazenar a figurinha no array
-        if(figuinha%2){
-            (*tamanhoMaria)++;
+int contaToca(int i, int loop, int tamanho, int myArray[]){
+	if(loop < tamanho){
+        int contagem = (myArray[i] == loop) ? 1 : 0;
+        if(contagem == 0){
+            return contagem + contaToca(i + 1, loop, tamanho, myArray);
         }else{
-            (*tamanhoJoao)++;
+            return contagem + contaToca(i, loop + i, tamanho, myArray);
         }
-        myFor(myArray, tamanho, tamanhoJoao, tamanhoMaria, loop+1);
     }
-    return ;
+	return 0;
+}
+void myFor(int myArray[], int loop, int tamanho){
+    if(loop<tamanho){
+	    scanf("%d", &myArray[loop]);
+    	myFor(myArray, loop + 1, tamanho);
+	}
+	return;
+
 }
 
 int main(){
-    int tamanho, tamanhoMaria=0, mariaPonto=0, tamanhoJoao=0, joaoPonto=0, maior=0;
-    scanf("%d", &tamanho);
-    int figurinhas[tamanho];
-    myFor(figurinhas, tamanho, &tamanhoJoao, &tamanhoMaria, 0);
-    repetida(figurinhas, tamanho, 0, 0);
-    calculandoFigurinhas(figurinhas, tamanho, &mariaPonto, &joaoPonto, 0);
-    if(mariaPonto>joaoPonto){
-        printf("%d\n%d\n%d",tamanhoMaria,tamanhoJoao,mariaPonto);
-    }else{
-        printf("%d\n%d\n%d",tamanhoMaria,tamanhoJoao,joaoPonto);
-    }
-    return 0;
+	int entrada;
+	scanf("%d", &entrada);
+	int tocas[entrada];
+	myFor(tocas, 0, entrada);
+	printf("%d", contaToca(0, 0, entrada, tocas));
+    retrun 0;
 }
