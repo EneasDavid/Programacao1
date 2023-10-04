fea2a473e153fc25d7fc0cd83bab5e03bd695f7f
Enéas <david00eneas@gmail.com>
2023-10-04 18:13:24 -0300

    melhoria na questão da princesa

--- Lista8/Matriz de votação.c
+++ Lista8/Matriz de votação.c
@@ -2,15 +2,13 @@
 
 int main() {
     int qtdPrincesas, qtdEleitores;
-    scanf("%d", &qtdPrincesas);
-    scanf("%d", &qtdEleitores);
+    scanf("%d %d", &qtdPrincesas, &qtdEleitores);
     int votos[qtdEleitores][qtdPrincesas];
     for (int linha=0; linha<qtdEleitores; linha++) {
         for (int coluna=0; coluna<qtdPrincesas; coluna++) {
             scanf("%d", &votos[linha][coluna]);
         }
     }
-
     for (int princesa=0; princesa<qtdPrincesas; princesa++) {
         int cont=0;
         for (int eleitor=0; eleitor<qtdEleitores; eleitor++) {
