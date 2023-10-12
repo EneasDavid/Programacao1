a8d15265b939743e3be8cc5c39b28e8b85e7b867
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2023-10-12 19:32:24 -0300

    Atualizar o Ekul e o Labirinto.c

--- Prova4/Ekul e o Labirinto.c
+++ Prova4/Ekul e o Labirinto.c
@@ -1,46 +1,42 @@
 #include <stdio.h>
 
-int dragoonPresent(int x, int y, int N, int maze[N][N]) {
-    for (int i=x-1; i<=x+1; i++) {
-        for (int j=y-1; j<=y+1; j++) {
-            if (i>=0 && i<N && j>=0 && j<N && maze[i][j]==2) return 1;
+int dragoaPresente(int x, int y, int N, int labirinto[N][N]) {
+    for (int i = x - 1; i <= x + 1; i++) {
+        for (int j = y - 1; j <= y + 1; j++) {
+            if (i >= 0 && i < N && j >= 0 && j < N && labirinto[i][j] == 2) return 1;
         }
     }
     return 0;
 }
 
-int canEscape(int x, int y, int N, int maze[N][N]) {
-    if (x<0 || x>=N || y<0 || y>=N) return 0;
-    if (dragoonPresent(x, y, N, maze)) return 0;
-    if (x==N-1 && y== N-1) return 1;
-    if (maze[x][y]==0 || maze[x][y]==3) return 0;
+int podeEscaparLabirinto(int x, int y, int N, int labirinto[N][N]) {
+    if (x < 0 || x >= N || y < 0 || y >= N) return 0;
+    if (x == N - 1 && y == N - 1) return 1;
+    if (labirinto[x][y] == 0 || labirinto[x][y] == 3) return 0;
+    labirinto[x][y] = 3;
 
-    maze[x][y]=3;
-
-    int dx[2]={1, 0};
-    int dy[2]={0, 1};
-    for (int i=0; i<2; i++) {
-        int nextX=x+dx[i];
-        int nextY=y+dy[i];
-        if (canEscape(nextX, nextY, N, maze)) return 1;
-    }
+    if (explorarCaminho(x + 1, y, N, labirinto) || explorarCaminho(x, y + 1, N, labirinto)) return 1;
     return 0;
+}
+
+int explorarCaminho(int x, int y, int N, int labirinto[N][N]) {
+    if (dragoaPresente(x, y, N, labirinto)) return 0;
+    return podeEscaparLabirinto(x, y, N, labirinto);
 }
 
 int main() {
     int N;
     scanf("%d", &N);
-    int mapaLabirinto[N][N];
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {
-            scanf("%d", &mapaLabirinto[i][j]);
+    int labirinto[N][N];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &labirinto[i][j]);
         }
     }
-    if (canEscape(0, 0, N, mapaLabirinto)) printf("Ekul conseguiu ludibriar a dragoa!\n");
+    if (podeEscaparLabirinto(0, 0, N, labirinto)) printf("Ekul conseguiu ludibriar a dragoa!\n");
     else printf("Ekul tomou o nhac da dragoa :(\n");
     return 0;
 }
-
 /*
 [|] [0] [0]
     
