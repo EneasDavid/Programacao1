e73ef77206967fb1e76b0fd96aae14e6ad59110f
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-02-05 14:03:07 -0300

    Otimização

--- Prova 1_23.2/Ekul e o campo de flores.c
+++ Prova 1_23.2/Ekul e o campo de flores.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
 int main(){
-    int horario, qtnHoraSono,inverno, verao, primavera, outono;
+    int horario, qtnHoraSono,inverno, verao, primavera, outono, disposicao;
     char estacao, clima;
     scanf("%d %d %c %c %d %d %d %d", &horario, &qtnHoraSono, &estacao, &clima, &inverno, &verao, &primavera, &outono);
-    //CHANCE = ESTA??O + (HORARIO EM HORAS % CLIMA) * TEMPO DE SONO
-    int vEstacao=estacao=='I'?inverno:estacao=='V'?verao:estacao=='P'?primavera:outono;
-    int vClima=clima=='E'?7:clima=='C'?5:3;
-    int disposicao=(qtnHoraSono/2)*19;
-    if(disposicao>=70){
-        int chance=vEstacao+((horario/60)%vClima)*qtnHoraSono;
-        if(chance>=(vEstacao*3) || chance>100){
-            printf("%d%%, Ekul possui chances de encontrar a flor :)", chance>100?100:chance);
+    disposicao=qtnHoraSono*9.5;
+    if(disposicao<70){
+        printf("0%%, Ekul nao acordou disposto hoje :(");
+    }else{
+        int vEstacao=estacao=='I'?inverno:estacao=='V'?verao:estacao=='P'?primavera:outono;
+        int vClima=clima=='E'?7:clima=='C'?5:3;
+        int chance=(vEstacao+((horario/60)%vClima)*qtnHoraSono)>100?100:chance;
+        if(chance>=vEstacao*3>100?100:vEstacao){
+            printf("%d%%, Ekul possui chances de encontrar a flor :)", chance);
         }else{
             printf("%d%%, Ekul nao possui chances de encontrar a flor :(", chance);
         }
-   }else{
-         printf("0%%, Ekul nao acordou disposto hoje :(");
-   }
+    }
     return 0;
 }
--- Prova 1_23.2/Pendrives e os descontos.c
+++ Prova 1_23.2/Pendrives e os descontos.c
@@ -1,20 +1,12 @@
-/*
-Estoque inicial é de 100 pendrives
-
-Se o cliente comprar até 5 pendrives, não há desconto.
-Se o cliente comprar de 6 a 10 pendrives, recebe um desconto de 5% no valor total da compra.
-Se o cliente comprar mais de 10 pendrives, recebe um desconto de 10% no valor total da compra.
-
-cada pendrive custa 5 reais.
-*/
-
 #include <stdio.h>
 
 int main(){
     int qtnPendrive;
     scanf("%d", &qtnPendrive);
-    if(qtnPendrive<=100){
-         double valorFinal;
+    if(qtnPendrive>100){
+        printf("Poxa, nao temos mais pendrives no estoque");
+    }else{
+        double valorFinal;
         if(qtnPendrive>6 && qtnPendrive<10){
             valorFinal=(qtnPendrive*5)*.95;
         }else if(qtnPendrive>10){
@@ -23,8 +15,6 @@ int main(){
             valorFinal=qtnPendrive*5;
         }
         printf("Ainda existe estoque! O valor total eh de R$ %.2lf e sobraram %d pendrives no estoque", valorFinal, (100-qtnPendrive));
-    }else{
-        printf("Poxa, nao temos mais pendrives no estoque");
     }
     return 0;
 }
