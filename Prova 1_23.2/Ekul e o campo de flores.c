858550b30fe568e505844500d508b811cdd0973e
Enéas <david00eneas@gmail.com>
2024-02-07 19:40:32 -0300

    Finalização

--- Prova 1_23.2/Ekul e o campo de flores.c
+++ Prova 1_23.2/Ekul e o campo de flores.c
@@ -9,8 +9,10 @@ int main(){
     }else{
         int vEstacao=estacao=='I'?inverno:estacao=='V'?verao:estacao=='P'?primavera:outono;
         int vClima=clima=='E'?7:clima=='C'?5:3;
-        int chance=(vEstacao+((horario/60)%vClima)*qtnHoraSono)>100?100:chance;
-        if(chance>=vEstacao*3>100?100:vEstacao){
+        int chance=(vEstacao+((horario/60)%vClima)*qtnHoraSono);
+        chance=chance>100?100:chance;
+        vEstacao=vEstacao*3>100?100:vEstacao;
+        if(chance>=vEstacao){
             printf("%d%%, Ekul possui chances de encontrar a flor :)", chance);
         }else{
             printf("%d%%, Ekul nao possui chances de encontrar a flor :(", chance);
